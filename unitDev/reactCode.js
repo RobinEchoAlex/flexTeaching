8f91663631eb1a7b2d6778bebfe9102fc4530c1d
RobinEchoAlex <872576923@qq.com>
2021-06-29 16:30:39 +0100

    reformat JS

--- unitDev/reactCode.js
+++ unitDev/reactCode.js
@@ -2,4 +2,9 @@ function a() {
     console.log("clicked")
 }
 
-ReactDOM.render(<button onClick={a}>Powered By React/JSX</button>, document.getElementById("app2"))
+ReactDOM.render(
+    <button onClick={a}>
+        Powered By React/JSX
+    </button>,
+    document.getElementById("app2")
+)
