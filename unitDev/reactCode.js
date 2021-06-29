4be47c184fd839a56c8f206f7e800278fdae3055
RobinEchoAlex <872576923@qq.com>
2021-06-29 20:39:23 +0100

    download function added

--- unitDev/reactCode.js
+++ unitDev/reactCode.js
@@ -1,12 +1,24 @@
 function responseDownload() {
+
     console.log("Download response")
+
+    var blobParts = [];
 
     document.querySelectorAll('[id^="input_"]').forEach(function (node){
             console.log(node.id);
-            console.log(node.value)
-
+            console.log(node.value);
+            var s = "<div id=" + node.id + ">" + node.value + "</div>"
+            blobParts.push(s)
         }
     );
+
+    var blob = new Blob(blobParts);
+    var aDownload = document.createElement('a');
+    var url = window.URL.createObjectURL(blob);
+    var filename = 'response.html';
+    aDownload.href = url;
+    aDownload.download = filename;
+    aDownload.click();
 
 }
 
