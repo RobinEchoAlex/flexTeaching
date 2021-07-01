14d6380c7d1a1b230f4531f4348c07e548e52fc2
RobinEchoAlex <872576923@qq.com>
2021-07-01 23:52:37 +0100

    Add code for ID input->html Download (in an inelegant way)

---
+++ inst/assignments/test1/include/js/reactCode.js
@@ -0,0 +1,39 @@
+function responseDownload() {
+
+    console.log("Download response")
+    var id = {id};
+    console.log(id);
+
+    var blobParts = [];
+    blobParts.push(id);
+
+    document.querySelectorAll('[id^="input_"]').forEach(function (node){
+            console.log(node.id);
+            console.log(node.value);
+            var s = "<div id=\"" + node.id + "\">" + node.value + "</div>"
+            blobParts.push(s)
+        }
+    );
+
+    var blob = new Blob(blobParts);
+    var aDownload = document.createElement('a');
+    var url = window.URL.createObjectURL(blob);
+    var filename = 'response.html';
+    aDownload.href = url;
+    aDownload.download = filename;
+    aDownload.click();
+
+}
+
+ReactDOM.render(
+    <div>
+        <input id='input_q1'/>
+        <br/>
+        <button onClick={responseDownload}>
+            Download your response
+        </button>
+    </div>
+    ,
+    document.getElementById("app2")
+)
+
