e98385f5e7f80d184deed665a39a99b1de9556cb
RobinEchoAlex <872576923@qq.com>
2021-07-02 10:13:43 +0100

    HTML download uses doc object r/t string concatenation

--- inst/assignments/test1/include/js/reactCode.js
+++ inst/assignments/test1/include/js/reactCode.js
@@ -4,18 +4,31 @@ function responseDownload() {
     var id = {id};
     console.log(id);
 
+    var doc = document.implementation.createHTMLDocument("New Document");
+
     var blobParts = [];
     blobParts.push(id);
+
+    var idDiv = doc.createElement('div')
+    idDiv.id = "id"
+    idDiv.textContent = id
+    doc.body.appendChild(idDiv)
 
     document.querySelectorAll('[id^="input_"]').forEach(function (node){
             console.log(node.id);
             console.log(node.value);
             var s = "<div id=\"" + node.id + "\">" + node.value + "</div>"
             blobParts.push(s)
+
+            var div = doc.createElement('div')
+            div.id=node.id
+            div.textContent= node.id + ":" + node.value
+            doc.body.appendChild(div);
         }
     );
 
-    var blob = new Blob(blobParts);
+    console.log(doc.body.innerHTML)
+    var blob = new Blob([doc.body.innerHTML]);
     var aDownload = document.createElement('a');
     var url = window.URL.createObjectURL(blob);
     var filename = 'response.html';
