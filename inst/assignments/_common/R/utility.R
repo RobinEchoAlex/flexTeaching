4e48534ba293765544c4cf8c0c256aae45040b29
RobinEchoAlex <872576923@qq.com>
2021-07-04 23:46:40 +0100

    Add comment

--- inst/assignments/_common/R/utility.R
+++ inst/assignments/_common/R/utility.R
@@ -1,22 +1,32 @@
 compile_assignment_html <- function(path, envir = new.env(), ...){
   tmpfn = tempfile(fileext = ".html")
   input = file.path(path, "index.Rmd")
+  # render as html_fragment, intended for inclusion with other web pages. So the page does not support embedded image etc.
+  # equal to 'output: html_fragment' in rmd header. see https://rmarkdown.rstudio.com/html_fragment_format.html
   output_format = rmarkdown::html_fragment(pandoc_args = c("--metadata", "title= " ) )
   rmarkdown::render(input = input, output_file = tmpfn, output_format = output_format,
                     envir = envir, quiet = TRUE, ...)
   return(tmpfn)
 }
 
+#' Generate the assignment data file for downloading.
+#' @usage included in the button list as onClick handler defined in utility.R in each assignment.
+#'         Eventually being binded at content.RMD.
+#' @return  a list containing the file name (fn), and data file (d).
 data_file = function(assignment_data, id, seed, solutions, format, init, entry){
-  df = init$data
+  df = init$data # raw data
   hash = digest::digest(df)
-  ext = flexTeaching:::formats[[format]]$ext
-  time = format(Sys.time(), "%d%m%Y_%H%M%S")
+  ext = flexTeaching:::formats[[format]]$ext #file extension
+  currentTimeString = format(Sys.time(), "%d%m%Y_%H%M%S")
+
+  # if it's 'Check your assignment' page, then include seed in filename
   fn = ifelse(entry=="solve",
               glue::glue("{entry}_{assignment_data$shortname}_{id}_{seed}_{time}_{hash}{ext}"),
               glue::glue("{entry}_{assignment_data$shortname}_{id}_{time}_{hash}{ext}")
               )
+  # generate a rand fileName
   tf = tempfile(fileext = ext)
+  # call corresponding format handler to write data to file
   flexTeaching:::formats[[format]]$f(df, tf)
   d = readBin(con = tf, what = "raw", n = file.size(tf))
   return(list(fn = fn, d = d))
